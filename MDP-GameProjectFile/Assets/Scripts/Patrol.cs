29116552454ede1c1efacaab35571d7f7e210f06
AO-101 <74131315+Pop-Locker101@users.noreply.github.com>
2021-06-21 12:26:41 +1000

    FINALLY!!!!!!!!!

--- MDP-GameProjectFile/Assets/Scripts/Patrol.cs
+++ MDP-GameProjectFile/Assets/Scripts/Patrol.cs
@@ -6,10 +6,54 @@ public class Patrol : MonoBehaviour
 {
     public GameObject SecurityGuard;
     public float speed;
+    public Vector3 startPosition;
+    public Vector3[] moveToPoints;
+    public Vector3 currentPoint;
 
-    public void Update()
+    public float moveSpeed;
+
+    public int pointSelection;
+
+    // Use this for initialization
+    void Start()
     {
-        float y = Mathf.PingPong(Time.time * speed, 1) * 6 - 3;
-        SecurityGuard.transform.position = new Vector3(0, y, 0);
+
+        //Sets the object to your starting point
+        this.transform.position = startPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Move();
+
+    }
+
+
+    void Move()
+    {
+
+        //Starts to move the object towards the first "moveToPoint" you set in inspector
+        this.transform.position = Vector3.MoveTowards(this.transform.position, currentPoint, Time.deltaTime * moveSpeed);
+
+        //check to see if the object is at the next "moveToPoint"
+        if (this.transform.position == currentPoint)
+        {
+
+            //if so it sets the next moveTo location
+            pointSelection++;
+
+            //if your object hits the last "moveToPoint it sends the object back to starting position to start the sequence over
+            if (pointSelection == moveToPoints.Length)
+            {
+                pointSelection = 0;
+
+            }
+
+            //sets the destination of the "moveToPoint" destination
+            currentPoint = moveToPoints[pointSelection];
+        }
     }
 }
