cd10701bce0d68fa99a45a00d22b1b72564ad919
AO-101 <atila.ozeken@education.nsw.gov.au>
2021-06-24 19:17:01 +1000

    po

--- MDP-GameProjectFile/Assets/Hiding.cs
+++ MDP-GameProjectFile/Assets/Hiding.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class Hiding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
